591f86153fd0615cba22b677808811504ee6ac90
Jaiswar <jaiswarnilesh2002@gmail.com>
2022-06-28 20:53:52 +0530

    updated

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,11 +1,43 @@
 import React from 'react'
 import Image from 'next/image'
-import { motion } from 'framer-motion' 
+import { motion } from 'framer-motion'
 
 import AboutImage from "../../illustrations/about.gif"
 import { BiDownload } from "react-icons/bi"
 
-const About = ({setDownloadPopUp}) => {
+const About = ({ setDownloadPopUp,winWidth }) => {
+  function isSmall(){
+    if(winWidth<=820){
+      return true
+    }else return false
+  }
+  const container={
+    hidden:{
+      opacity:0
+    },
+    visible:{
+      opacity:1,
+      transition:{
+        staggerChildren:0.4
+      }
+    }
+  }
+
+  const item={
+    hidden:{
+      opacity:0,
+      y:30,
+      // x:15
+    },
+    visible:{
+      opacity:1,
+      y:0,
+      // x:0,
+      transition:{
+        duration:1
+      }
+    }
+  }
   return (
     <section className="jais-port__about" id='aboutme'>
       <div className="jais-port__about-title">
@@ -18,21 +50,23 @@ const About = ({setDownloadPopUp}) => {
         </div>
       </div>
       <div className="jais-port__about-content">
-        <div className="jais-port__about-content__image">
+        <motion.div className="jais-port__about-content__image" initial={{ x: isSmall() ? 0 :  -100,y:isSmall() ? 100 :0, opacity: 0}} whileInView={{x:0,y:0,opacity:1}} viewport={{once:true}} transition={{duration:1}}>
           <Image src={AboutImage} width={600} height={600} alt="Image" />
-        </div>
-        <div className="jais-port__about-content__detail">
+        </motion.div>
+        <motion.div className="jais-port__about-content__detail" variants={container} initial="hidden" whileInView={"visible"} viewport={{once:true}}>
           <div className="para">
-            <p>My Name is <span className="highlighter">Nilesh Jaiswar.</span> I am Full Stack Developer, and I am very passionate about coding and building professtional stuff.</p>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo corrupti impedit, tempora expedita molestias labore debitis a, officiis ad eligendi, fugiat totam? Illo natus suscipit ut ipsam consectetur sequi? Dolor.</p>
+            <motion.p variants={item}>My Name is <span className="highlighter">Nilesh Jaiswar.</span> I am Full Stack Developer, and I am very passionate about coding and building professtional stuff.</motion.p>
+            <motion.p variants={item}>I am keen to learn new technology and implement in my projects. I dropped my college after second year to fully focus on things I passionate about. I read books to enhance my outer world Knowledge. I created several projects
+              for both national and international companies.
+            </motion.p>
           </div>
           <div className="hire__btn">
-            <motion.div className="btn" whileTap={{scale:0.95}} onClick={()=>setDownloadPopUp(true)}>
-              <BiDownload size={ 25} />
+            <motion.div className="btn" whileTap={{ scale: 0.95 }} onClick={() => setDownloadPopUp(true)} variants={item}>
+              <BiDownload size={25} />
               <h3>Resume</h3>
             </motion.div>
           </div>
-        </div>
+        </motion.div>
       </div>
     </section>
   )
--- src/components/HeroComp.jsx
+++ src/components/HeroComp.jsx
@@ -12,24 +12,52 @@ const HeroComp = ({winWidth}) => {
       return true
     }else return false
   }
+
+  const container={
+    hidden:{
+      opacity:0
+    },
+    visible:{
+      opacity:1,
+      transition:{
+        staggerChildren:0.4
+      }
+    }
+  }
+
+  const item={
+    hidden:{
+      opacity:0,
+      y:30,
+      // x:15
+    },
+    visible:{
+      opacity:1,
+      y:0,
+      // x:0,
+      transition:{
+        duration:1
+      }
+    }
+  }
   return (
     <section className="jais-port__hero" id='home'>
-      <div className="jais-port__hero-info">
-        <div className="line1">
-          <h2>Hello There,</h2>
-          <h1>I am <span>
+      <motion.div className="jais-port__hero-info" variants={container} initial={"hidden"} whileInView={"visible"} viewport={{once:true}}>
+        <motion.div className="line1" >
+          <motion.h2 variants={item}> Hello There,</motion.h2>
+          <motion.h1 variants={item}>I am <span>
             Nilesh Jaiswar.
-          </span></h1>
-          <p>I am a Full-Stack Developer, Basically MERN Stack and Next JS Developer. Based in <span>Mumbai</span>.</p>
-          <p>Got Some Idea for your next <span>Project</span>?</p>
-        </div>
+          </span></motion.h1>
+          <motion.p variants={item}>I am a Full-Stack Developer, Basically MERN Stack and Next JS Developer. Based in <span>Mumbai</span>.</motion.p>
+          <motion.p variants={item}>Got Some Idea for your next <span>Project</span>?</motion.p>
+        </motion.div>
         <div className="jais-port__hero-contact">
-          <motion.div className="btn" whileTap={{ scale: 0.95 }} onClick={()=>router.push("#contact")} >
+          <motion.div className="btn" variants={item} whileTap={{ scale: 0.95 }} onClick={()=>router.push("#contact")} >
             <h3>Let&lsquo;s Talk</h3>
           </motion.div>
         </div>
-      </div>
-      <motion.div className="jais-port__hero-image" initial={{ x: isSmall() ? 0 :  100,y:isSmall ? 100 :0, opacity: 0 }} animate={{ x: 0,y:0 , opacity: 1 }} transition={{ duration: 1 }}>
+      </motion.div>
+      <motion.div className="jais-port__hero-image" initial={{ x: isSmall() ? 0 :  100,y:isSmall() ? 100 :0, opacity: 0 }} animate={{ x: 0,y:0 , opacity: 1 }} transition={{ duration: 1 }}>
         <Image src={AnimatedGif} width={700} height={700}  priority={true} alt="Gif" />
       </motion.div>
     </section>
